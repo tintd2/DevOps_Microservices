b3ec293ddb6cf07ce78eaf8419dca920d319fa9a
TranDinhTin <tdt.smile1@gmail.com>
2023-07-11 00:22:56 +0700

    k8s complete

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -16,6 +16,6 @@ docker image ls
 # Step 3: 
 # Run flask app
 # docker run -d -p 8000:80 dinhtin12/devops
-docker run -d  -v $(pwd)/output_txt_files:/app/output_txt_files -p 8000:80 --name devops dinhtin12/devops:1.0.0
+docker run -d  -v "$(pwd)"/output_txt_files:/app/output_txt_files -p 8000:80 --name devops dinhtin12/devops:1.0.0
 docker ps -a
 # docker logs devops > output_txt_files/docker_out.txt
--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -9,18 +9,27 @@ dockerpath=dinhtin12/devops
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run k8s-devops --image=dinhtin12/devops:1.0.0 --port=5001
+kubectl delete pod k8s-devops
+kubectl run k8s-devops --image=dinhtin12/devops:1.0.0 --port=80
 
 # Step 3:
 # List kubernetes pods
-unset http_proxy
-unset https_proxy
+# unset http_proxy
+# unset https_proxy
 kubectl get pods -n default
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward k8s-devops 7080:5001
+# kubectl wait -n defautl --for=condition=ready pod --all
+# kubectl wait --for=condition=ready pod -l app=k8s-devops 
+# kubectl wait --for=condition=running --timeout=30s  pod --all
+kubectl wait --for=condition=Ready pod/k8s-devops
+kubectl port-forward k8s-devops 8000:80
+
+# minikube delete
+# minikube stop
 
 
 # kubectl get pods -n default
 # kubectl describe pods
+# kubectl logs 
